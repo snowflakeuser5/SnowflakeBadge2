11590f72aeb68001377447776305fb16bac30256
snowflakeuser5 <150109175+snowflakeuser5@users.noreply.github.com>
2023-11-06 22:55:12 -0700

    Update streamlit_app.py
    Added Toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('my parents new healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard boiled free range egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard boiled free range egg')
+streamlit.text('🥑🍞 Avacado toast')
